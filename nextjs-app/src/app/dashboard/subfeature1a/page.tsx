ac5c82377cb12c9950a9da9176b26e79f2a38244
Kasper Rasmussen <obscur@tuta.io>
2025-09-04 17:12:29 +0200

    trAIn subfeature 1a

---
+++ nextjs-app/src/app/dashboard/subfeature1a/page.tsx
@@ -0,0 +1,8 @@
+import WorkoutCard from '@/app/ui/train/WorkoutCard';
+const Subfeature1APage = () => {
+  return (<div>hey this is my gym training page.
+  <WorkoutCard />
+  </div>);
+}
+
+export default Subfeature1APage;
